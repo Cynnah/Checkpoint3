3a31db1cdbae57764e1d342b65a514571b106373
Cynnah <massot.dorian@gmail.com>
2018-06-15 10:18:04 +0200

    add a list of targets

---
+++ src/Components/ListTargets.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { ListGroup } from "reactstrap";
+import Targets from "./Targets";
+
+const ListTargets = () => (
+  <ListGroup>
+    <Targets />
+    <Targets />
+    <Targets />
+    <Targets />
+  </ListGroup>
+);
+
+export default ListTargets;
